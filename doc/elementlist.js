81e7558469933f86ba91c42f3ee127105afc52c7
Ethan Ruffing <ruffinge@gmail.com>
2014-08-01 16:06:09 -0400

    Updated documentation

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","exception_error_handler()"],["f","filter_request()"],["f","filter_session()"]];
+ApiGen.elements = [["f","exception_error_handler()"],["f","filter_request()"],["f","filter_session()"],["f","format_stack_trace()"]];
